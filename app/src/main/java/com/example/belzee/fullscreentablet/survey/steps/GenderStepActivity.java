f9806f44192fc6f494f534638c3408761d389216
Kajetan Lipensky <klipensk@akamai.com>
2018-08-02 17:45:41 +0200

    working on table/grid for buttons

--- app/src/main/java/com/example/belzee/fullscreentablet/survey/steps/GenderStepActivity.java
+++ app/src/main/java/com/example/belzee/fullscreentablet/survey/steps/GenderStepActivity.java
@@ -2,13 +2,15 @@ package com.example.belzee.fullscreentablet.survey.steps;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.example.belzee.fullscreentablet.FullscreenActivity;
 import com.example.belzee.fullscreentablet.R;
 
-@SuppressLint("Registered")
 public class GenderStepActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,5 +20,15 @@ public class GenderStepActivity extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.gender_layout);
+    }
+
+    public void onMenBtnClick(View view) {
+        Intent nextStep = new Intent(GenderStepActivity.this, YearsStepActivity.class);
+        GenderStepActivity.this.startActivity(nextStep);
+    }
+
+    public void onWomenBtnClick(View view) {
+        Intent nextStep = new Intent(GenderStepActivity.this, YearsStepActivity.class);
+        GenderStepActivity.this.startActivity(nextStep);
     }
 }
--- app/src/main/java/com/example/belzee/fullscreentablet/survey/steps/YearsStepActivity.java
+++ app/src/main/java/com/example/belzee/fullscreentablet/survey/steps/YearsStepActivity.java
@@ -2,13 +2,19 @@ package com.example.belzee.fullscreentablet.survey.steps;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.TextView;
 
 import com.example.belzee.fullscreentablet.R;
 
-@SuppressLint("Registered")
 public class YearsStepActivity extends Activity {
 
     @Override
@@ -19,5 +25,49 @@ public class YearsStepActivity extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.years_layout);
+
+        GridView gridView = (GridView)findViewById(R.id.gridview);
+
+        gridView.addView();
     }
+
+    public static class YearsAdapter extends BaseAdapter {
+
+        private final Context mContext;
+        private final Button[] buttons;
+
+        // 1
+        public YearsAdapter(Context context, Button[] buttons) {
+            this.mContext = context;
+            this.buttons = buttons;
+        }
+
+        // 2
+        @Override
+        public int getCount() {
+            return buttons.length;
+        }
+
+        // 3
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        // 4
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        // 5
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TextView dummyTextView = new TextView(mContext);
+            dummyTextView.setText(String.valueOf(position));
+            return dummyTextView;
+        }
+
+    }
+
 }
