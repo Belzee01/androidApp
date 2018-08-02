d9e4c6b11427117afc359d95af3781c93ac07c46
Kajetan Lipensky <klipensk@akamai.com>
2018-08-02 17:26:20 +0200

    made first step of survey

--- app/src/main/java/com/example/belzee/fullscreentablet/FullscreenActivity.java
+++ app/src/main/java/com/example/belzee/fullscreentablet/FullscreenActivity.java
@@ -1,32 +1,28 @@
 package com.example.belzee.fullscreentablet;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.FrameLayout;
+
+import com.example.belzee.fullscreentablet.survey.steps.GenderStepActivity;
 
 public class FullscreenActivity extends Activity {
-
-    final FrameLayout frameLayout = (FrameLayout)findViewById(R.id.main_layout);
-
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        frameLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_fullscreen);
+    }
+
+    public void backgroundClick(View view) {
+        Intent nextStep = new Intent(FullscreenActivity.this, GenderStepActivity.class);
+        FullscreenActivity.this.startActivity(nextStep);
     }
 }
